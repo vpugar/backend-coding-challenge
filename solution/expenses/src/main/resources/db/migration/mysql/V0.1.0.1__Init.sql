5071a7981263185e3baab76add76f2ee457bfdaa
vpugar <vpugar@gmail.com>
2018-10-21 10:12:47 +0200

    Missing previously ignored sql and application properties

--- solution/expenses/src/main/resources/db/migration/mysql/V0.1.0.1__Init.sql
+++ solution/expenses/src/main/resources/db/migration/mysql/V0.1.0.1__Init.sql
@@ -11,10 +11,23 @@ CREATE UNIQUE INDEX currency_idx_01
 
 CREATE TABLE exchange_rate
 (
-  id                  BIGINT AUTO_INCREMENT PRIMARY KEY
+  id                  BIGINT AUTO_INCREMENT PRIMARY KEY,
+  rate_date           DATE            NOT NULL,
+  source_currency_id  BIGINT          NOT NULL,
+  target_currency_id  BIGINT          NOT NULL,
+  rate                DECIMAL(14, 8)  NOT NULL
 )
   ENGINE = InnoDB;
 
+CREATE UNIQUE INDEX exchange_rate_idx_01
+  ON exchange_rate(rate_date, source_currency_id, target_currency_id);
+
+ALTER TABLE exchange_rate
+  ADD CONSTRAINT fk_exchange_rate_source_currency_id
+  FOREIGN KEY (source_currency_id) REFERENCES currency(id);
+ALTER TABLE exchange_rate
+  ADD CONSTRAINT fk_exchange_rate_target_currency_id
+  FOREIGN KEY (target_currency_id) REFERENCES currency(id);
 
 CREATE TABLE expense_user
 (
@@ -24,8 +37,40 @@ CREATE TABLE expense_user
 )
   ENGINE = InnoDB;
 
+CREATE UNIQUE INDEX expense_user_idx_01
+  ON expense_user(username);
+CREATE UNIQUE INDEX expense_user_idx_02
+  ON expense_user(email);
+
 CREATE TABLE expense
 (
-  id                  BIGINT AUTO_INCREMENT PRIMARY KEY
+  id                  BIGINT AUTO_INCREMENT PRIMARY KEY,
+  created_at          TIMESTAMP       NOT NULL,
+  expense_date        DATE            NOT NULL,
+  reason              VARCHAR(1000)   NOT NULL,
+  source_currency_id  BIGINT          NOT NULL,
+  source_amount       DECIMAL(14, 2)  NOT NULL,
+  currency_id         BIGINT          NOT NULL,
+  amount              DECIMAL(14, 2)  NOT NULL,
+  vat_rate            DECIMAL(14, 2)  NOT NULL,
+  vat_amount          DECIMAL(14, 2)  NOT NULL,
+  user_id             BIGINT          NOT NULL
 )
   ENGINE = InnoDB;
+
+CREATE INDEX expense_idx_01
+  ON expense(user_id);
+CREATE INDEX expense_idx_02
+  ON expense(expense_date);
+CREATE INDEX expense_idx_03
+  ON expense(created_at);
+
+ALTER TABLE expense
+  ADD CONSTRAINT fk_expense_source_currency_id
+  FOREIGN KEY (source_currency_id) REFERENCES currency(id);
+ALTER TABLE expense
+  ADD CONSTRAINT fk_expense_currency_id
+  FOREIGN KEY (currency_id) REFERENCES currency(id);
+ALTER TABLE expense
+  ADD CONSTRAINT fk_expense_user_id
+  FOREIGN KEY (user_id) REFERENCES expense_user(id);
---
+++ solution/expenses/src/main/resources/db/migration/mysql/V0.1.0.2__Init_data.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO currency(short_name, scale) VALUES ('GBP', 2);
+INSERT INTO currency(short_name, scale) VALUES ('EUR', 2);
+INSERT INTO currency(short_name, scale) VALUES ('USD', 2);
+INSERT INTO currency(short_name, scale) VALUES ('CHF', 2);
+
+INSERT INTO expense_user(username, email) VALUES ('test', 'test@test.com');
